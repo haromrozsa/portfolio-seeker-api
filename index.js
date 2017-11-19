12ed29728354f8a9e732ea8b3f9dddf8667601fb
haromrozsa <haromrozsa@gmail.com>
2017-11-19 11:38:36 +0100

    Init commit

--- index.js
+++ index.js
@@ -6,12 +6,127 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var scheduler = require('node-schedule');
-var api_key = process.env.MAILGUN_API_KEY;
+
+var api_key = "gfdgf";//process.env.MAILGUN_API_KEY;
 var domain = 'sandbox21dd732e255747b48e88245c204feda6.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
-var url = 'https://forum.portfolio.hu/topics/opus-global-nyrt/25754?limit=100'
-//var url = 'https://forum.portfolio.hu/topics/opus-global-nyrt/25754?oldal=453&limit=100'
+var siteNumber;
+var tags = [];
+var tagsCount = {};
+var tagsWithCount = [];
+var date = new Date();
+var dateFormated = date.getFullYear() + "." + date.getMonth() + "." + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
+var url = 'https://forum.portfolio.hu/topics/opus-global-nyrt/25754?limit=100';
+var emaildata = {
+  from: 'Excited User <me@samples.mailgun.org>',
+  to: 'haromrozsa@gmail.com',
+  subject: 'Hello',
+  text: 'Testing some Mailgun awesomness!'
+};
+
+var parseToDate = function(dateString) {
+	//var dateString = dateString.replace("é", "e");
+	//console.log(dateString);
+		//if (dateString == "pentek") {
+		//	console.log("nana");
+		//	}
+		 var d = new Date(); // today!
+
+		 var currentDayInt;
+		 if (dateString.indexOf("tf")!= -1) { //hétfõ
+			 	currentDayInt = 1;
+		 } else if (dateString.indexOf("kedd")!= -1) { //
+		 	currentDayInt = 2;
+		} else if (dateString.indexOf("szerda")!= -1) { //szerda
+		 	currentDayInt = 3;
+		} else if (dateString.indexOf("cs")!= -1) { //csütörtök
+		 	currentDayInt = 4;
+		} else if (dateString.indexOf('ntek')!= -1) { //péntek
+			 //console.log("nana");
+		 	currentDayInt = 5;
+		} else if (dateString.indexOf("szombat")!= -1) { //szombat
+		 	currentDayInt = 6;
+		} else if (dateString.indexOf("vas")!= -1) { //vasárnap
+		 	currentDayInt = 0;
+		} else if (dateString.indexOf("ma")!= -1) { //ma
+ 		 	return d.getFullYear() + "." + d.getMonth() + "." + d.getDate()
+ 		 } else  {
+		 		return dateString;
+		 }
+
+		 //console.log(currentDayInt);
+		 /*switch (dateString.trim()) {
+	    case "hétfõ":
+	        currentDayInt = 1;
+	        break;
+	    case "kedd":
+	        currentDayInt = 2;
+	        break;
+	    case "szerda":
+	        currentDayInt = 3;
+	        break;
+	    case "csütörtök":
+	        currentDayInt = 4;
+	        break;
+	    case "péntek":
+	//				console.log("hello");
+	        currentDayInt = 5;
+	        break;
+	    case "szombat":
+	        currentDayInt = 6;
+	        break;
+	    case "vasárnap":
+	        currentDayInt = 0;
+					break;
+			default:
+					return dateString;
+			}*/
+
+		 var dateDay = (currentDayInt - new Date().getDay()) < 0 ? (new Date().getDay() - currentDayInt) : (new Date().getDay() - currentDayInt + 7);
+		 d.setDate(d.getDate() - dateDay);
+		 //console.log(d.getFullYear() + "." + d.getMonth() + "." + d.getDate());
+		 return d.getFullYear() + "." + d.getMonth() + "." + d.getDate();
+};
+
+var parseMonthToDate = function(dateString) {
+		var splitted = dateString.split(" ");
+		if (splitted.length == 2) {
+
+				var monthInt;
+				if (splitted[0].trim().indexOf("jan")!= -1) { //hétfõ
+ 				 	monthInt = 1;
+ 			 } else if (splitted[0].trim().indexOf("feb")!= -1) { //
+ 			 	monthInt = 2;
+ 			} else if (splitted[0].trim().indexOf("rciu")!= -1) { //szerda
+ 			 	monthInt = 3;
+ 			} else if (splitted[0].trim().indexOf("prilis")!= -1) { //csütörtök
+ 			 	monthInt = 4;
+ 			} else if (splitted[0].trim().indexOf('jus')!= -1) { //péntek
+ 				 //console.log("nana");
+ 			 	monthInt = 5;
+ 			} else if (splitted[0].trim().indexOf("nius")!= -1) { //szombat
+ 			 	monthInt = 6;
+ 			} else if (splitted[0].trim().indexOf("lius")!= -1) { //vasárnap
+ 			 	monthInt = 7;
+ 			} else if (splitted[0].trim().indexOf("augusz")!= -1) { //ma
+ 	 		 	monthInt = 8;
+			} else if (splitted[0].trim().indexOf("szept")!= -1) { //ma
+  	 		 	monthInt = 9;
+  	  	} else if (splitted[0].trim().indexOf("okt")!= -1) { //ma
+ 	 		 	monthInt = 10;
+			} else if (splitted[0].trim().indexOf("novem")!= -1) { //ma
+  	 		 	monthInt = 11;
+  	 	} else if (splitted[0].trim().indexOf("decem")!= -1) { //ma
+	  	 		 	monthInt = 12;
+	  	 } else  {
+ 			 		return dateString;
+ 			 }
+				return new Date().getFullYear() + "." + monthInt + "." + splitted[1].trim();
+
+		}
+		return dateString;
+};
 
 var sendEmail = function(result) {
 	    var dateNew = new Date();
@@ -22,8 +137,6 @@ var sendEmail = function(result) {
 		});
 };
 
-var date = new Date();
-var dateFormated = date.getFullYear() + "." + date.getMonth() + "." + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
 var montlyJob = scheduler.scheduleJob('10 01 * * *', function() { //*/1 * * * *
  console.log('I am going to send an email on ' + dateFormated);
 
@@ -35,12 +148,6 @@ var montlyJob = scheduler.scheduleJob('10 01 * * *', function() { //*/1 * * * *
 
 });
 
-
-
-var siteNumber;
-var tags = [];
-var tagsCount = {};
-var tagsWithCount = [];
 var parseResponse = function(response) {
   var data = "";
   response.on('data', function(chunk) {
@@ -54,31 +161,35 @@ var parseResponse = function(response) {
 		//console.log(siteHref); //eq(3).children().text());
 
 		siteNumber = siteHref.split('?').pop().split('&').shift().split('=').pop();
-		//console.log(siteNumber);	
+		//console.log(siteNumber);
 	} else  {
 		siteNumber--;
 	}
-	
+
 	//$('div .date').each(function(i, elem) {
 	$('div .upRow').children('div .date').each(function(i, elem) {
 		var dateText = $(this).text();
 		var dateTextSplitted;// = dateText.split(",");
-		if (dateText.indexOf(",") != -1) { 
-			dateTextSplitted = dateText.split(",");
+		if (dateText.indexOf(",") != -1) {
+			//dateTextSplitted = dateText.split(",")[0].trim();
+			//parseToDate(dateTextSplitted[0].trim());
+			//console.log(dateText.split(",")[0].trim());
+			//dateTextSplitted = parseToDate("péntek");
+			dateTextSplitted = (parseToDate(dateText.split(",")[0].trim()));
 		} else {
-			dateTextSplitted = dateText.split("|");
+			dateTextSplitted = parseMonthToDate(dateText.split("|")[0].trim());
 		}
 		//console.log(dateTextSplitted[0]);
-		if(tags.indexOf(dateTextSplitted[0].trim()) === -1) {
-			tags.push(dateTextSplitted[0].trim());
-			tagsCount[dateTextSplitted[0].trim()] = 1;
+		if(tags.indexOf(dateTextSplitted) === -1) {
+			tags.push(dateTextSplitted);
+			tagsCount[dateTextSplitted] = 1;
 		} else {
-			tagsCount[dateTextSplitted[0].trim()]++;
+			tagsCount[dateTextSplitted]++;
 		}
 	  //fruits[i] = $(this).text();
 	});
-	
-	if (siteNumber > 0) {
+
+	if (siteNumber > 520) {
 		//console.log(siteNumber);
 		var newUrl = url + "&oldal=" + siteNumber;
 		console.log(newUrl);
@@ -87,14 +198,14 @@ var parseResponse = function(response) {
 		})
 	} else {
 		//var myJsonString = JSON.stringify(yourArray);
-		//console.log($('div .date').length); 
+		//console.log($('div .date').length);
 		for(var i = 0; i < tags.length;i++) {
 			tagsWithCount.push({date:tags[i], count:tagsCount[tags[i]]});
 		}
 		//var result = tagsWithCount;
 		var result = JSON.stringify(tagsWithCount);
 	    //console.log(tagsWithCount[0].count);
-		console.log(result); 
+		console.log(result);
 		sendEmail(result);
 		/*var obj = JSON.parse(result);
 		var objdates = [];
@@ -107,12 +218,16 @@ var parseResponse = function(response) {
 		console.log(objcounter);*/
 	}
 
-		//console.log($('div .date').length);   
-		//console.log(tagsCount); 
+		//console.log($('div .date').length);
+		//console.log(tagsCount);
   });
 }
 
- 
+https.get(url, function(response) {
+	console.log("Test loaded " + url);
+	parseResponse(response);
+});
+
 app.get('/opus', cors(), function (req, res, next) {
   console.log("Request arrived");
   res.json(JSON.stringify(tagsWithCount));
@@ -125,16 +240,9 @@ app.get('/', cors(), function (req, res, next) {
   //emaildata.subject = 'Hello ' + dateNew.getFullYear() + "." + dateNew.getMonth() + "." + dateNew.getDate() + " " + dateNew.getHours() + ":" + dateNew.getMinutes();
   //mailgun.messages().send(emaildata, function (error, body) {
   //	  console.log(body);
-  //});  
+  //});
   res.json('Request arrived OK');
 });
-
-var emaildata = {
-  from: 'Excited User <me@samples.mailgun.org>',
-  to: 'haromrozsa@gmail.com',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomness!'
-};
 
 app.get('/email', cors(), function (req, res, next) {
   console.log("Start sending email");
@@ -143,22 +251,21 @@ app.get('/email', cors(), function (req, res, next) {
 	});
   res.json('Email sent');
 });
- 
-const port = process.env.PORT || 8081; 
+
+const port = process.env.PORT || 8081;
 app.listen(port, function () {
   console.log('CORS-enabled web server listening, date: ' + dateFormated);
 });
 /*http.createServer(function (request, response) {
-   // Send the HTTP header 
+   // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    console.log("Request arrived");
    response.writeHead(200, {'Content-Type': 'application/json'});
-   
+
    // Send the response body as "Hello World"
    response.end(JSON.stringify(tagsWithCount));
 }).listen(8081);*/
 
 // Console will print the message
 //console.log('Server running at http://127.0.0.1:8081/');
-
